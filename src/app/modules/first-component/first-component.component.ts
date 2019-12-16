7ed9f34945d0b7dd6764903b57dabb115e535e9b
Camara <victordsgnr@gmail.com>
2019-12-16 13:35:22 -0300

    configuração inicial

---
+++ src/app/modules/first-component/first-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html'
+})
+export class FirstComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/first-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FirstModulesRoutingModule } from './first-route.routing';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FirstModulesRoutingModule
+  ]
+})
+export class FirstModulesModule { }
